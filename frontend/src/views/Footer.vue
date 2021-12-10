df91c3a2a34b14f11a45823150a1d1384aaaf754
darkborsh <n_ragnar@mail.ru>
2021-12-10 14:04:50 +0300

    footer added.

---
+++ frontend/src/views/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <section class="links">
+      <div>
+        <router-link to="/">Donate</router-link>
+        <router-link to="/">AniList.co</router-link>
+      </div>
+      <div>
+        <router-link to="/">Apps</router-link>
+        <router-link to="/">Recommendations</router-link>
+      </div>
+      <div>
+        <router-link to="/">Discord</router-link>
+        <router-link to="/">GitHub</router-link>
+        <router-link to="/">VK</router-link>
+      </div>
+      <div>
+        <router-link to="/">Terms & Privacy</router-link>
+        <router-link to="/">Moderators</router-link>
+      </div>
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style src="../assets/style/footer-style.css">
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
-  <Header></Header>
+  <section>
+    <Header></Header>
+    <Footer></Footer>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 
 import Header from "./Header"
+import Footer from "./Footer";
 
 export default {
   name: 'Home',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
